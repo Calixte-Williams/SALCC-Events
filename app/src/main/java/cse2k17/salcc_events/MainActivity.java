01689dd3356db95ec4f16bd5015b7ccf9d962e2a
Calixte Williams <trigger_402@hotmail.com>
2017-03-10 16:43:41 -0400

    First Git Commit to initialize Version Control

---
+++ app/src/main/java/cse2k17/salcc_events/MainActivity.java
@@ -0,0 +1,13 @@
+package cse2k17.salcc_events;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
