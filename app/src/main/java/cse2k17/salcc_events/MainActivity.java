c8394c3b173d472acfe86de070fd3ad9854e8b20
Calixte Williams <trigger_402@hotmail.com>
2017-03-10 18:36:26 -0400

    #3 Added Toast on click of a date

--- app/src/main/java/cse2k17/salcc_events/MainActivity.java
+++ app/src/main/java/cse2k17/salcc_events/MainActivity.java
@@ -1,7 +1,16 @@
 package cse2k17.salcc_events;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialCalendarView materialCalendarView = (MaterialCalendarView) findViewById(R.id.calendarView);
+
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.MONDAY)
+                .setMinimumDate(CalendarDay.from(2016, 12, 31))
+                .setMaximumDate(CalendarDay.from(2020, 12, 31))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Toast.makeText(MainActivity.this, "" + date, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
